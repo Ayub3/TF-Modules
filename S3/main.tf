89af90a65369c59eab220f47f06b4a2c023d7800
Ayub Mohamud <ayubmohamud@Ayubs-MacBook-Pro.local>
2025-07-30 18:30:21 +0100

    feat: add s3 module

--- S3/main.tf
+++ S3/main.tf
@@ -1,9 +1,95 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.aws_s3.bucket_name
-  force_destroy = var.aws_s3.force_destroy
-  
+  bucket              = var.bucket_name
+  force_destroy       = var.aws_s3.force_destroy
+  object_lock_enabled = var.object_lock_enabled
 
-   tags = {
-    Name = merge(var.aws_s3_bucket.tags, local.tags)
+  tags = {
+    Name = merge(var.tags, local.tags)
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = var.aws_s3_bucket_ownership
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = var.versioning_status
+  }
+}
+
+resource "aws_kms_key" "s3_bucket_kms_key" {
+  count                   = var.enable_sse ? 1 : 0
+  deletion_window_in_days = var.deletion_window_in_days
+  enable_key_rotation     = var.enable_key_rotation
+  policy                  = var.kms_key_policy_json
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  count  = var.enable_sse ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_bucket_kms_key.arn
+      sse_algorithm     = var.kms_key_id != null ? "aws:kms" : "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_configuration" {
+  count  = length(var.lifecycle_rules) > 0 ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  dynamic "rule" {
+    for_each = var.lifecycle_rules
+    content {
+      id     = rule.value.id
+      status = rule.value.status
+
+      dynamic "filter" {
+        for_each = rule.value.prefix != null ? [rule.value] : []
+        content {
+          prefix = rule.value.prefix
+        }
+      }
+
+      expiration {
+        days = rule.value.expiration.days
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  count  = var.enable_website ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = var.index_document
+  }
+
+  error_document {
+    key = var.error_document
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  count      = var.bucket_policy_enabled ? 1 : 0
+  bucket     = aws_s3_bucket.bucket.id
+  policy     = var.bucket_policy_json
+  depends_on = [aws_s3_bucket_public_access_block.block_public_access]
 }
--- S3/variables.tf
+++ S3/variables.tf
@@ -1,7 +1,121 @@
-variable "aws_s3" {
-  type = object({
-    bucket_name                = string
-    force_destroy         = string
-    tags                   = map(string)
-  })
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
+
+variable "object_lock_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "aws_s3_bucket_ownership" {
+  description = "Ownership control for objects. Options: BucketOwnerPreferred, ObjectWriter, BucketOwnerEnforced."
+  type        = string
+  default     = "BucketOwnerEnforced"
+}
+
+variable "block_public_acls" {
+  type    = bool
+  default = true
+}
+
+variable "block_public_policy" {
+  type    = bool
+  default = true
+}
+
+variable "ignore_public_acls" {
+  type    = bool
+  default = true
+}
+
+variable "restrict_public_buckets" {
+  type    = bool
+  default = true
+}
+
+variable "versioning_status" {
+  type    = string
+  default = "enabled"
+}
+
+variable "enable_sse" {
+  type    = bool
+  default = true
+}
+
+variable "kms_key_id" {
+  type    = string
+  default = null
+}
+
+variable "deletion_window_in_days" {
+  type    = number
+  default = 10
+}
+
+variable "enable_key_rotation" {
+  type    = bool
+  default = true
+}
+
+variable "lifecycle_rules" {
+  description = "List of S3 lifecycle rules"
+  type = list(object({
+    id     = string
+    status = string
+    prefix = optional(string)
+    tags   = optional(map(string))
+    expiration = object({
+      days = number
+    })
+  }))
+  default = []
+}
+
+variable "bucket_policy_enabled" {
+  description = "Whether to attach a bucket policy"
+  type        = bool
+  default     = false
+}
+
+variable "bucket_policy_json" {
+  description = "JSON bucket policy to attach"
+  type        = string
+  default     = ""
+}
+
+variable "kms_key_policy_json" {
+  description = "KMS key JSON policu to attach"
+  type        = string
+  default     = ""
+}
+
+variable "enable_website" {
+  description = "Enable S3 static website hosting"
+  type        = bool
+  default     = false
+}
+
+variable "index_document" {
+  description = "The name of the index document for the website (e.g., index.html)"
+  type        = string
+  default     = "index.html"
+}
+
+variable "error_document" {
+  description = "The name of the error document for the website (e.g., error.html)"
+  type        = string
+  default     = "404.html"
 }
